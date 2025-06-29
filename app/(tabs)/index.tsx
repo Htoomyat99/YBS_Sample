908d96002968ccddc40b6b62d8b36ae64c874726
Htoo Myat <htoomyat.20399@gmail.com>
2025-06-30 01:07:22 +0630

    add folder structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,77 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { locationList } from "@/LocationList";
+import { AppleMaps, GoogleMaps } from "expo-maps";
+import React, { useState } from "react";
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useBottomTabOverflow } from "../../src/components/ui/TabBarBackground.ios";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const bottom = useBottomTabOverflow();
+  const [locationIndex, setLocationIndex] = useState(0);
+
+  const cameraPosition = {
+    coordinates: {
+      latitude: locationList[locationIndex].stores[0].point[0],
+      longitude: locationList[locationIndex].stores[0].point[1],
+    },
+    zoom: 10,
+  };
+
+  const renderMapControls = () => (
+    <>
+      <View style={{ flex: 8 }} pointerEvents="none">
+        <View style={styles.controlsContainer} pointerEvents="auto">
+          <Button
+            title="Prev"
+            onPress={() => setLocationIndex(locationIndex - 1)}
+          />
+
+          <Button
+            title="Next"
+            onPress={() => setLocationIndex(locationIndex + 1)}
+          />
+        </View>
+      </View>
+    </>
   );
+
+  if (Platform.OS === "ios") {
+    return (
+      <>
+        <AppleMaps.View
+          style={StyleSheet.absoluteFill}
+          cameraPosition={cameraPosition}
+        />
+        <SafeAreaView
+          style={{ flex: 1, paddingBottom: bottom }}
+          pointerEvents="box-none"
+        >
+          {/* {renderMapControls()} */}
+        </SafeAreaView>
+      </>
+    );
+  } else if (Platform.OS === "android") {
+    return (
+      <GoogleMaps.View style={{ flex: 1 }} cameraPosition={cameraPosition} />
+    );
+  } else {
+    return <Text>Maps are only available on Android and iOS</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  controlsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
 });
